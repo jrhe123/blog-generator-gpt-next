026cec250ee6e8f39b33dbeb4c185783c4115ca0
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-24 18:21:03 -0400

    display user info

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["lh3.googleusercontent.com"],
+		formats: ["image/avif", "image/webp"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
 import Link from "next/link";
+import Image from "next//image";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+	const { user } = useUser();
+	console.log("user: ", user);
 	return (
 		<main>
 			<div>home page here</div>
 			<div>
-				<Link href={"/api/auth/login"}>login</Link>
+				{!!user ? (
+					<>
+						<div>
+							{user.picture && (
+								<Image
+									src={user.picture}
+									alt={user.name || ""}
+									height={50}
+									width={50}
+								/>
+							)}
+							<div>{user.email}</div>
+						</div>
+						<Link href={"/api/auth/logout"}>logout</Link>
+					</>
+				) : (
+					<Link href={"/api/auth/login"}>login</Link>
+				)}
 			</div>
 		</main>
 	);

1e9de634c0ee4a0a05abfbfb35cf140f1c18bb6d
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-25 15:51:08 -0400

    replace bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Logo } from "../components/logo";
-import HeroImage from "../../public/hero.webp";
+import BackgroundImage from "../../public/Nasdaq-stock-exchange.webp";
 
 export default function Home() {
 	return (
 		<div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
-			<Image src={HeroImage} alt="Hero" fill className="absolute" />
+			<Image src={BackgroundImage} alt="NASDAQ" fill className="absolute" />
 			<div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
 				<Logo />
 				<p>

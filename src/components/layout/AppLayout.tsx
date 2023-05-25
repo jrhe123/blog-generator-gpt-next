aa397ca9fe1d41c2a2d5d807d792c13b4f0e72d4
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-24 22:34:04 -0400

    style side bar

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next";
-
+import Link from "next/link";
+import Image from "next//image";
+//
+import { useUser } from "@auth0/nextjs-auth0/client";
 interface IAppLayoutProps {
 	children: React.ReactNode;
 }
@@ -11,9 +14,46 @@ interface IAppLayoutProps {
  */
 
 export const AppLayout: NextPage<IAppLayoutProps> = ({ children }) => {
+	const { user } = useUser();
+	console.log("user: ", user);
+
 	return (
-		<div>
-			<p>this is app layout</p>
+		<div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+			<div className="flex flex-col text-white overflow-hidden">
+				<div className="bg-slate-800">
+					<div>logo</div>
+					<div>cta</div>
+					<div>tokens</div>
+				</div>
+				<div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+					list of posts
+				</div>
+				<div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+					{!!user ? (
+						<>
+							<div className="min-w-[50px]">
+								{user.picture && (
+									<Image
+										src={user.picture}
+										alt={user.name || ""}
+										height={50}
+										width={50}
+										className="rounded-full"
+									/>
+								)}
+							</div>
+							<div className="flex-1">
+								<div className="font-bold">{user.email}</div>
+								<Link className="text-sm" href={"/api/auth/logout"}>
+									logout
+								</Link>
+							</div>
+						</>
+					) : (
+						<Link href={"/api/auth/login"}>login</Link>
+					)}
+				</div>
+			</div>
 			<div>{children}</div>
 		</div>
 	);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,8 @@
-import Link from "next/link";
-import Image from "next//image";
-import { useUser } from "@auth0/nextjs-auth0/client";
-
 export default function Home() {
-	const { user } = useUser();
-	console.log("user: ", user);
 	return (
 		<main>
 			<div>home page here</div>
-			<div>
-				{!!user ? (
-					<>
-						<div>
-							{user.picture && (
-								<Image
-									src={user.picture}
-									alt={user.name || ""}
-									height={50}
-									width={50}
-								/>
-							)}
-							<div>{user.email}</div>
-						</div>
-						<Link href={"/api/auth/logout"}>logout</Link>
-					</>
-				) : (
-					<Link href={"/api/auth/login"}>login</Link>
-				)}
-			</div>
+			<div></div>
 		</main>
 	);
 }

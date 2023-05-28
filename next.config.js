622e51ea429bd3246f16d1b5757ce4f083db43d3
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-28 11:56:18 -0400

    add cors on images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["lh3.googleusercontent.com"],
+		domains: ["lh3.googleusercontent.com", "s.gravatar.com"],
 		formats: ["image/avif", "image/webp"],
 	},
 };

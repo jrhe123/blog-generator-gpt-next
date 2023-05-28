eb37ea25e5d8e2fd0f082875aac695cd8a78e7b0
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-27 20:53:51 -0400

    new post ui

--- src/pages/post/new.tsx
+++ src/pages/post/new.tsx
@@ -6,6 +6,9 @@ import { useRouter } from "next/router";
 import { AppLayout } from "../../components/layout";
 import { ObjectId } from "mongodb";
 import getAppProps from "@/utils/getAppProps";
+//
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBrain } from "@fortawesome/free-solid-svg-icons";
 
 interface INewPostProps {}
 
@@ -18,7 +21,6 @@ const NewPost: NextPageWithLayout = (props) => {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [topic, setTopic] = useState<string>("");
 	const [keywords, setKeywords] = useState<string>("");
-	// const [postContent, setPostContent] = useState<string>("");
 
 	const handleCreatePost = async (e: React.FormEvent) => {
 		try {
@@ -46,54 +48,60 @@ const NewPost: NextPageWithLayout = (props) => {
 				message?: string;
 			} = await response.json();
 			if (jsonResponse.code === 0 && jsonResponse.postId) {
-				// const post = jsonResponse.post;
-				// setPostContent(post?.postContent || "");
 				router.push(`/post/${jsonResponse.postId}`);
 			}
 		} catch (error) {
 			console.log("error: ", error);
-		} finally {
 			setIsLoading(false);
 		}
 	};
 
 	return (
-		<div>
-			<form onSubmit={handleCreatePost}>
-				<div>
-					<label>
-						<strong>Generate a blog post on the topic of:</strong>
-					</label>
-					<textarea
-						className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-						value={topic}
-						onChange={(e) => setTopic(e.target.value)}
-					/>
+		<div className="h-full overflow-hidden">
+			{isLoading ? (
+				<div className="text-green-500 flex h-full animate-pulse w-full flex-col justify-center items-center">
+					<FontAwesomeIcon icon={faBrain} className="text-8xl" />
+					<h6>Generating...</h6>
 				</div>
-				<div>
-					<label>
-						<strong>Targeting the following keywords:</strong>
-					</label>
-					<textarea
-						className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
-						value={keywords}
-						onChange={(e) => setKeywords(e.target.value)}
-					/>
+			) : (
+				<div className="w-full h-full flex flex-col overflow-auto">
+					<form
+						onSubmit={handleCreatePost}
+						className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+					>
+						<div>
+							<label>
+								<strong>Generate a blog post on the topic of:</strong>
+							</label>
+							<textarea
+								className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+								value={topic}
+								onChange={(e) => setTopic(e.target.value)}
+							/>
+						</div>
+						<div>
+							<label>
+								<strong>Targeting the following keywords:</strong>
+							</label>
+							<textarea
+								className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+								value={keywords}
+								onChange={(e) => setKeywords(e.target.value)}
+							/>
+							<small className="block mb-2">
+								Separate keywords with a comma
+							</small>
+						</div>
+						<button
+							type="submit"
+							className="btn"
+							disabled={isLoading || !keywords || !topic}
+						>
+							Generate
+						</button>
+					</form>
 				</div>
-				<button
-					type="submit"
-					className="btn"
-					disabled={isLoading || !keywords || !topic}
-				>
-					Generate
-				</button>
-			</form>
-			{/* <div
-				className="max-w-screen-sm p-10"
-				dangerouslySetInnerHTML={{
-					__html: postContent,
-				}}
-			/> */}
+			)}
 		</div>
 	);
 };
